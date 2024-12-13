752cea617a2b26125dd642354ac49140bcef4237
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2024-12-13 20:28:09 -0300

    Update styles.ts

--- src/components/PostItem/styles.ts
+++ src/components/PostItem/styles.ts
@@ -1,30 +1,35 @@
-import styled from 'styled-components'
+    import styled from 'styled-components'
 
-export const ContainerPost = styled.div`
-    border: 1px solid var(--background-color2);
-    display: flex;
-    padding: 10px;
-    margin: 10px 0;
-    gap: 20px;
+    export const ContainerPost = styled.div`
+        border: 1px solid var(--background-color2);
+        display: flex;
+        padding: 10px;
+        margin: 10px 0;
+        gap: 20px;
 
-    img{
-        width: 120px;
-        height: 80px;
-    }
-    
-    p{
-        font-size: small;
-    }
-    button{
-        background-color: red;
-        padding: 2px 3px;
-        color: #fff;
-        width: 100%;
-        border: 1px solid var(--text-color);    
-    }
+        img{
+            width: 120px;
+            height: 80px;
+        }
+        
+        p{
+            font-size: small;
+        }
+        button{
+            background-color: red;
+            padding: 2px 3px;
+            color: #fff;
+            width: 100%;
+            border: 1px solid var(--text-color);    
+        }
 
-    strong{
-        color: cadetblue;
-    }
+        strong{
+            color: cadetblue;
+        }
 
-`;
+        @media (max-width: 1020px){
+            display: flex;
+            flex-direction: column-reverse;
+
+        }
+    `;
