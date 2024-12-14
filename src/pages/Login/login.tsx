b6c8adba406a83fd221902af2cac4c584e534053
lauanderson-rael <lauanderson38@gmail.com>
2024-12-14 18:35:42 -0300

    vv

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -12,7 +12,7 @@ const Login: React.FC = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('http://localhost:3000/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
