8e81477d8b1d41a2c048a4a0e7115a897f98e99d
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2024-12-15 11:40:18 -0300

    Atualizar o login.tsx

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -39,7 +39,7 @@ const Login: React.FC = () => {
     <Container>
     <form onSubmit={handleSubmit}>
       <h1>Bem vindo de volta</h1>
-      <span>acesse com seu usuario e senha para ter acesso ao <b>sistema administrador</b> do blog</span>
+      <span>Entre com seu usuario e senha para ter acesso ao <b>sistema administrador</b> do blog</span>
       <label>usuario:</label>
       <input
         type="text"
