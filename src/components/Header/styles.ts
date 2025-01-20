0f0789be0dfea6f35aaeb4686ec280b2cd8b6be4
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2025-01-20 14:49:11 -0300

    transition in header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,6 +8,7 @@ const VerificarRota = () => {
 }
 
 export const Container = styled.header`
+  transition: all 1s ease-out;
   background-color: var(--background-color-gray);
   color: var(--text-color);
   box-shadow: 0 -15px 25px 10px var(--text-color);
